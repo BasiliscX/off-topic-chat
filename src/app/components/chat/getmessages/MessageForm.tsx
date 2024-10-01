9a7b59428cdc8698dad2f31c90c23b3d305a85c0
BasiliscX <juniornavarro1988@gmail.com>
2024-10-01 18:50:46 -0300

    Refactor chat component and control panel styles: Adjust height of message form container and add card styling

--- src/app/components/chat/getmessages/MessageForm.tsx
+++ src/app/components/chat/getmessages/MessageForm.tsx
@@ -1,8 +1,8 @@
 import MessageList from "./MessageList";
 export default function MessageForm() {
   return (
-    <div className="flex-grow md:w-0 bg-slate-200 shadow-md rounded-lg">
-      <div className="p-4 bg-gray-100 rounded-t-lg">
+    <div className="flex-grow md:w-0 bg-slate-200 shadow-md rounded-lg card">
+      <div className="p-4 rounded-t-lg">
         <h2 className="text-xl font-bold">Mensajes</h2>
       </div>
 

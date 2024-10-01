5d46050bc0445d826e352228667bc343df1970fa
BasiliscX <juniornavarro1988@gmail.com>
2024-10-01 15:57:42 -0300

    Refactor chat component and control panel styles

--- src/app/components/chat/ControlPanel.tsx
+++ src/app/components/chat/ControlPanel.tsx
@@ -1,11 +1,18 @@
 export default function ControlPanel() {
   return (
-    <div className="flex flex-col mt-10 md:mt-0 md:w-1/4 space-y-4">
+    <div className="flex flex-col justify-center mt-10 md:mt-0 md:w-1/4  space-y-4">
+      <input
+        type="text"
+        placeholder="Anon"
+        className="p-2 bg-gray-200 rounded-md"
+      />
+      <textarea
+        placeholder="Mensaje"
+        className="p-4 bg-gray-200 rounded-md"
+        rows={4}
+      />
       <button type="button" className="p-2 bg-blue-500 text-white rounded-md">
-        Nuevo mensaje
-      </button>
-      <button type="button" className="p-2 bg-blue-500 text-white rounded-md">
-        Borrar mensajes
+        Enviar mensaje
       </button>
     </div>
   );

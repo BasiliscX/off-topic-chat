78bfaf149a6146b8c2ac0ff421733e6fc94238b2
BasiliscX <juniornavarro1988@gmail.com>
2024-10-02 19:26:39 -0300

    Refactor page component: Update metadata for Off-Topic Chat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,33 @@
 import Chat from "./components/chat/Chat";
 
+export const metadata = {
+  title: "Off-Topic Chat - Join the Conversation!",
+  description:
+    "Engage in lively discussions and share your thoughts on Off-Topic Chat.",
+  keywords: ["chat", "off-topic", "conversations", "discussions"],
+  openGraph: {
+    title: "Off-Topic Chat - Join the Conversation!",
+    description:
+      "Engage in lively discussions and share your thoughts on Off-Topic Chat.",
+    url: "https://offtopic-alpha.vercel.app",
+    images: [
+      {
+        url: "/icon.png",
+        width: 800,
+        height: 600,
+        alt: "Off-Topic Chat",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Off-Topic Chat - Join the Conversation!",
+    description: "Join Off-Topic Chat for interesting discussions.",
+    images: ["/images/icon.webp"],
+  },
+};
+
 export default function Home() {
   return (
     <>
